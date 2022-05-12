cae9403f9b0a3e79306253adb61e241102c60d95
mrjleo <mrjleo@users.noreply.github.com>
2022-05-12 14:22:43 +0200

    add Google scholar link

---
+++ src/_data/links.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "name": "GitHub",
+        "url": "https://github.com/mrjleo",
+        "icon": "fa-brands fa-github"
+    },
+    {
+        "name": "Google Scholar",
+        "url": "https://scholar.google.com/citations?user=bEVcJjMAAAAJ",
+        "icon": "fa-solid fa-graduation-cap"
+    },
+    {
+        "name": "Twitter",
+        "url": "https://twitter.com/mr_jleo",
+        "icon": "fa-brands fa-twitter"
+    },
+    {
+        "name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/jurek-leonhardt-4176a3176/",
+        "icon": "fa-brands fa-linkedin"
+    }
+]
+  
